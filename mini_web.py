922ab46f0b218c714f63b31a4ea2ba1cb0490cdc
scrappyzhang <a7478317@163.com>
2018-03-07 02:57:36 +0800

    修改bug:int(len(resquest_header.encode()))

--- mini_web.py
+++ mini_web.py
@@ -2,7 +2,6 @@ from gevent import monkey
 
 monkey.patch_all()
 
-"""遵循WSGI的mini-web服务器"""
 '''
 @Time    : 2017/11/28 下午7:06
 @Author  : scrappy_zhang
@@ -23,29 +22,16 @@ monkey.patch_all()
 
 当遇到动态请求时，则通过app本身来实现，即后端。
 """
-# 进程
-import multiprocessing
 import socket
-import re
 import gevent
-import sys
-import time
-import os
-
-STATIC_ROOT = './static'  # 静态目录
 
 
 class HTTPServer(object):
     def __init__(self, port):
         """初识化操作"""
-        # 1 创建TCP套接字
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # 2 设置地址重用选项-让服务器重启之后可以理解重新使用绑定的端口
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-        # 2 绑定端口 监听  取客户端
-        server_socket.bind(('', port))  #
+        server_socket.bind(('', port))
 
         server_socket.listen(128)  # 128表示可监听最大连接数
 
@@ -72,25 +58,27 @@ class HTTPServer(object):
         request_line = ''  # 请求行
         while True:
             # 完整读取请求数据
-            recv_data_s = client_socket.recv(1024)
+            recv_data_s = client_socket.recv(256)
+            print('接收长度为', len(recv_data_s))
             if recv_data_s == b'':
                 request_line = '0'
                 return request_line, request_headers, content_entity
             try:
                 recv_data_s = recv_data_s.decode()
-                print(recv_data_s)
             except Exception as e:
-                print('》》》'+ str(e))
-                print(recv_data_s)
-                recv_data_s = recv_data_s.decode('gb2312')
+                recv_data_s = recv_data_s.decode('gbk')
 
+            if "\r\n\r\n" not in recv_data:
+                recv_data += recv_data_s
+            if "\r\n\r\n" not in recv_data:
+                pass
 
-            if "\r\n\r\n" in recv_data_s:
-                # 有空行
+            else:
                 if request_header == '':
-                    space_line_index = recv_data_s.index("\r\n\r\n")
-                    request_header = recv_data_s[0: space_line_index]
-                    content_entity = recv_data_s[space_line_index + 4:]
+                    # 第一次接收到， 空行 说明请求头结束
+                    space_line_index = recv_data.index("\r\n\r\n")
+                    request_header = recv_data[0: space_line_index]
+                    content_entity = recv_data[space_line_index + 4:]
                     for index, request in enumerate(request_header.split('\r\n')):
                         if index == 0:
                             request_line = request
@@ -101,31 +89,22 @@ class HTTPServer(object):
                             value = value.strip(' ')
                             request_headers[key] = value
 
-                    # 第一次接收到，空行 说明请求头结束
-                    if "content_length" in request_headers.keys():
+                    if "content-length" in request_headers.keys():
                         # 查看content_length是否在请求头,若在，需要获取其值
-                        content_length = request_headers['content_length']
+                        content_length = int(request_headers['content-length'])
                         if content_length == len(content_entity.encode()):
                             print("接收请求数据完毕")
                             return request_line, request_headers, content_entity
                     else:
                         # 不存在则说明只有请求头,没有实体
                         return request_line, request_headers, content_entity
+                else:
+                    # 实体数据
+                    content_entity += recv_data_s
+                    if content_length == len(content_entity.encode()):
+                        print("接收请求数据完毕")
+                        return request_line, request_headers, content_entity
 
-                else:
-                    content_entity += recv_data_s
-                    if content_length == len(content_entity.encode()):
-                        print("接收请求数据完毕")
-                        return request_line, request_headers, content_entity
-            else:
-                # 没空行
-                if request_header == '':
-                    recv_data += recv_data_s
-                else:
-                    content_entity += recv_data_s
-                    if content_length == len(content_entity.encode()):
-                        print("接收请求数据完毕")
-                        return request_line, request_headers, content_entity
 
     def deal_with_request(self, client_socket, client_addr):
         """使用这个函数进行服务"""
@@ -136,13 +115,13 @@ class HTTPServer(object):
             client_socket.close()
             return
         print(request_line)
-        content = '<p><strong>'+request_line + '</strong></p><hr>'
+        content = '<p><strong>' + request_line + '</strong></p><hr>'
         for key, value in request_headers.items():
             print(key + ':' + value)
-            content += '<p><span style="color:red; background-color:aliceblue">'+ key + '</span> : ' + value + '</p>'
+            content += '<p><span style="color:red; background-color:aliceblue">' + key + '</span> : ' + value + '</p>'
         if content_entity:
             print(content_entity)
-            content += '<hr><p>'+ content_entity + '</p>'
+            content += '<hr><p>' + content_entity + '</p>'
 
         str1 = """
                 <!DOCTYPE html>
@@ -155,7 +134,7 @@ class HTTPServer(object):
             """
         # 演示post请求
         str2 = """<hr>
-        <h3>POST测试:修改源码处的method值</h3>
+        <h3>POST测试:修改源码处的method值或者recv的大小值来测试各种场景</h3>
     <!-- action 为要将数据提交到哪个url -->
     <!-- method 为发送数据时的请求方式 get post等 -->
     <form action="http://localhost:8888/1" method="post">
@@ -181,7 +160,7 @@ class HTTPServer(object):
         resp_headers += "Server: PWB" + '\r\n'  # 告诉浏览器服务器
         resp_headers += "Content-Type: text/html;charset:utf-8;"
         resp_headers += '\r\n'  # 空行隔开body
-        resp_body = str1 + '<h1 style="color:white;background-color:blue">请从终端窗口查看相应数据并对比是否一致</h1>' + content + str2+ str3
+        resp_body = str1 + '<h1 style="color:white;background-color:blue">请从终端窗口查看相应数据并对比是否一致</h1>' + content + str2 + str3
         resp_data = resp_headers + resp_body
 
         client_socket.sendall((resp_data).encode())
